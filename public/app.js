c219296ef923379dcc8af67dc3ece7cd5fe587ab
Kriseta Timmas <kriseta.timmas@tktk.ee>
2025-05-29 14:17:22 +0300

    add week15 files

---
+++ public/app.js
@@ -0,0 +1,19 @@
+const images = ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG'];
+let i = 1;
+
+const targetImage = document.querySelector('.avatar img');
+
+targetImage.addEventListener('click', () => {
+    targetImage.style.opacity = '0';
+
+    setTimeout(() => {
+        targetImage.src = 'images/' + images[i];
+        i++;
+
+        if (i >= images.length) {
+            i = 0;
+        }
+
+        targetImage.style.opacity = '1';
+    }, 200);
+});
